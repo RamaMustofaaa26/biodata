2a26b7e00978049bbd0cbb437182059f8b8b7bd0
Rama Mustofa <ramskuyyy203@gmail.com>
2026-02-25 15:42:41 +0700

    biodata

---
+++ biodata.php
@@ -0,0 +1,35 @@
+<?php
+
+$nama = "Ramz";
+$umur = 17;
+$alamat = "Jakarta";
+$hobi = ["Membaca", "Ngoding", "Gaming"];
+
+if ($umur >= 18) {
+    $kategori = "Dewasa";
+} else {
+    $kategori = "Remaja";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata Saya</title>
+</head>
+<body>
+    <h2>Biodata Diri</h2>
+    <p><strong>Nama:</strong> <?= $nama ?></p>
+    <p><strong>Umur:</strong> <?= $umur ?> tahun</p>
+    <p><strong>Kategori:</strong> <?= $kategori ?></p>
+    <p><strong>Alamat:</strong> <?= $alamat ?></p>
+    p><strong>BUKU:</strong> <?= $buku ?></p>
+    
+    <p><strong>Hobi:</strong></p>
+    <ul>
+        <?php foreach ($hobi as $h): ?>
+            <li><?= $h ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
